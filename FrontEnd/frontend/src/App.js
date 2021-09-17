c719666404061ee8e461aa9004b26c13531b8982
kostaskompostas <59083148+kostaskompostas@users.noreply.github.com>
2021-09-17 21:53:33 +0300

    created most of the wireframes

--- FrontEnd/frontend/src/App.js
+++ FrontEnd/frontend/src/App.js
@@ -4,8 +4,9 @@ import  axios from "axios";
 import Welcome  from './LoggedOut/Welcome';
 import React from 'react';
 
+axios.defaults.baseURL = "http://192.168.1.7:8000"
 function App() {
-
+  
   return (
     <div className="App">
       <header className="App-header">
