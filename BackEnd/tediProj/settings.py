5ebd7fd7e9aa52323c5bc26376b0f9aa959689b1
kostaskompostas <59083148+kostaskompostas@users.noreply.github.com>
2021-09-24 19:10:14 +0300

    trying to fix stuff

--- BackEnd/tediProj/settings.py
+++ BackEnd/tediProj/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-a^3t_$j+u)!@v$)^_q(5(s4nk2mw=4zmulg%%ips%3ddfy$u93
 DEBUG = True
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 ALLOWED_HOSTS = ['localhost','192.168.1.35']
+=======
+ALLOWED_HOSTS = ['localhost','192.168.1.26','127.0.0.1']
+>>>>>>> Stashed changes
 =======
 ALLOWED_HOSTS = ['localhost','192.168.1.26','127.0.0.1']
 >>>>>>> Stashed changes
